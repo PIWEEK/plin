4fa9848af4f1d709f61c98843ea7513a04f3ba86
Yamila Moreno <yamila.ms@gmail.com>
2020-12-15 14:36:12 +0100

    (back) Add nested DRF endpoints

--- back/plin/urls.py
+++ back/plin/urls.py
@@ -19,16 +19,24 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_nested import routers
 
-from trips.viewsets import TripViewSet
+from trips.viewsets import TripViewSet, PlanViewSet, DayViewSet
 from users.viewsets import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 router.register(r'trips', TripViewSet, basename='trip')
 
+# Automatic routes for nested viewsets
+trips_router = routers.NestedSimpleRouter(router, r'trips', lookup='trip')
+trips_router.register(r'plans', PlanViewSet, basename='plan')
+trips_router.register(r'days', DayViewSet, basename='day')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(trips_router.urls)),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- back/trips/viewsets.py
+++ back/trips/viewsets.py
@@ -3,12 +3,11 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from trips.serializers import TripSerializer, LightTripSerializer
-from trips.models import Trip
+from trips.serializers import TripSerializer, LightTripSerializer, PlanSerializer, DaySerializer
+from trips.models import Trip, Plan, Day
 
 
 class TripViewSet(viewsets.ModelViewSet):
-    serializer_class = TripSerializer
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -18,3 +17,13 @@ class TripViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.request.user.trips.all()
+
+
+class PlanViewSet(viewsets.ModelViewSet):
+    serializer_class = PlanSerializer
+    queryset = Plan.objects.all()
+
+
+class DayViewSet(viewsets.ModelViewSet):
+    serializer_class = DaySerializer
+    queryset = Day.objects.all()
