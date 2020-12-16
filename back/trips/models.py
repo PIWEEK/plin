2a063051d19d8b33775af485bb9fb8b544319470
Yamila Moreno <yamila.ms@gmail.com>
2020-12-16 09:09:49 +0100

    Improve models

--- back/trips/models.py
+++ back/trips/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 
 
 class Trip(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
     duration = models.IntegerField(blank=True, null=False)
     from_date = models.DateField(blank=True, null=True)
     members = models.ManyToManyField(User, related_name="trips")
@@ -13,14 +14,21 @@ class Trip(models.Model):
     url_picture = models.URLField(blank=True, null=True)
     finished = models.BooleanField(blank=False, null=False, default=False)
 
+    class Meta:
+        ordering = ['from_date',]
+
     def __str__(self):
         return self.title
 
 
 class Day(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
     date = models.DateField(blank=True, null=True)
     order = models.IntegerField()
     trip = models.ForeignKey(Trip, related_name="days", on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['order',]
 
     def __str__(self):
         return f"{self.trip}: {self.order}"
@@ -28,14 +36,19 @@ class Day(models.Model):
 
 class Plan(models.Model):
     address = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     day = models.ForeignKey(Day, related_name="plans", blank=True, null=True, on_delete=models.SET_NULL)
     duration = models.IntegerField()
     google_id = models.CharField(max_length=100, blank=True, null=True)
     name = models.CharField(max_length=100, blank=False, null=False)
     opening_hours = models.CharField(max_length=100, blank=True, null=True)
+    order = models.IntegerField()
     popular_times = models.URLField(blank=True, null=True)
     trip = models.ForeignKey(Trip, related_name="plans", on_delete=models.CASCADE)
     url_picture = models.URLField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['order', 'created_at', ]
 
     def __str__(self):
         return self.name
