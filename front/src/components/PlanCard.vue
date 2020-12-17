693ba9c7ccb1869d7a3d8553cf1aa8e58638ca46
Pablo Alba <pablo.alba@kaleidos.net>
2020-12-17 12:07:00 +0100

    Add avatar to plan card

--- front/src/components/PlanCard.vue
+++ front/src/components/PlanCard.vue
@@ -15,17 +15,17 @@
     style="height: 4rem; margin-top:0.2rem; pointer-events: none;"    
     >
     <b-card-text>
-    <div>
-      {{ plan.name }}
-    </div>
-    <div>
-      <span v-if="plan.price !== null">{{ plan.price }}</span>
-      <span v-else>&nbsp;</span>
-    </div>
-    <div>
-      <span v-if="plan.duration !== null">🕑 {{ plan.duration }}</span>
-      <span v-else>&nbsp;</span>
-    </div>
+      <div>
+        {{ plan.name }}
+      </div>
+      <div>
+        <div v-if="plan.price !== undefined">{{ plan.price }}</div>
+        <div v-else style="color: white">.</div>
+      </div>
+      <div style="width: 100%">
+        <div style="width: 85%; display: inline-block">🕑 {{ plan.duration }}h</div>
+        <div style="width: 15%; display: inline-block; text-align: center"><b-img :src="gravatar('pablo.alba@kaleidos.net')" style="height:1.1rem;" rounded="circle"></b-img></div>
+      </div>
 
     </b-card-text>
   </b-card>
@@ -63,6 +63,11 @@
           const plan = evt.dataTransfer.getData('planId');
           console.log("Drop over card "+plan);
           this.$emit('plan_drop_on_child', plan, this.plan.id);
+        },
+        gravatar(email) {
+          var md5 = require('md5');
+          const hash = md5(email)
+          return "http://www.gravatar.com/avatar/"+hash;
         }
       }
     }
@@ -70,18 +75,24 @@
 
 <style lang="scss" scoped>
 .card-body {
-  padding: 0.3rem 0 0 0;
+  padding: 0.3rem 0 0 0.1rem;
   flex: 0.1 1 auto;
   font-size: 14px;
   text-align: left;
+  white-space: nowrap; 
+  width: 100%; 
+  overflow: hidden;
+  text-overflow: ellipsis; 
 }
 
 .card-text {
-  margin-left: 3px;  
+  margin-left: 3px;
+  line-height: 1.2rem;
+  text-overflow: ellipsis;
 }
 
 .card-img-left {
-  border-right: 4px solid blue;
+  border-right: 4px solid #7f7dd2;
   width: 4rem;
 }
 
