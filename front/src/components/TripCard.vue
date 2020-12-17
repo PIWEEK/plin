88daacdd970ff0d7df44d220b9e00091338c0690
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2020-12-17 23:50:44 +0100

    Default images for trips

--- front/src/components/TripCard.vue
+++ front/src/components/TripCard.vue
@@ -1,40 +1,35 @@
 <template>
   <b-card
-    style="width: calc(25% - 10px); margin: 5px; height:20rem; cursor: pointer"
-    class="image"
+    style="width: calc(25% - 20px); margin: 10px; height:15rem; cursor: pointer"
+    class="image shadow-sm"
     v-on:click="selectTrip"
     top
     :img-src="url_picture"
     img-alt="Image"
-    :title="title"
   >
+    <b-card-title>{{title}}</b-card-title>
 
     <b-card-text>
-    <!--
-      <h4 style="height: 4rem">
-        {{ title }}
-      </h4>
-      <div>
-    -->
-        <span v-if="start">Del {{ start }} </span>
-        <span v-if="end">al {{ end }} </span>
-        <div>
-          {{ duration }} días
-        </div>
-    <!--
-      </div>
-    -->
+      <span class="font-weight-bold" v-if="start">{{ prettyPrintDate(start) }} </span>
+      <span class="font-weight-bold" v-if="end">- {{ prettyPrintDate(end) }} </span>
+      ({{ duration }} días)
     </b-card-text>
   </b-card>
 </template>
 
 <script>
+    import moment from 'moment';
+
     export default {
       props: ["id", "title", "url_picture", "start", "end", "duration"],
       methods: {
         selectTrip: function () {
           this.$store.commit('SET_CURRENTTRIP', {id:this.id, title: this.title, url_picture: this.url_picture});
           this.$router.push("/trip");
+        },
+        prettyPrintDate: function (d) {
+          moment.locale('es');
+          return moment(d).format("D MMM");
         }
       }
     }
@@ -42,7 +37,22 @@
 
 <style lang="scss" scoped>
 .image img {
-  height: 140px;
+  height: 160px;
   object-fit: cover;
+}
+
+.card {
+  text-align: center;
+}
+
+.card-body {
+  padding: 0.75rem 1.25rem;
+}
+
+.card-title {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-bottom: 4px;
 }
 </style>
